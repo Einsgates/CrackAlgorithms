6b73c6d2dc62c3469937d3193e9d358534e629cd
Jiayuan Hong <einsgatehong@gmail.com>
2023-03-18 16:07:36 -0500

    cmb

---
+++ Backtracking/Combinations_77.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Combinations_77 {
+  public List<List<Integer>> combine(int n, int k) {
+    List<List<Integer>> res = new ArrayList<>();
+    
+    return res;
+  }
+
+  public static void main(String[] args) {
+    Combinations_77 myclasss = new Combinations_77();
+    int n = 4, k = 2;
+    System.out.println(myclasss.combine(n, k));
+  }
+}
